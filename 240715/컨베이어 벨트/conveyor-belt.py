2938ad510edaba18a323946f3d00829751d16e2e
soseonghyeon <103256915+soseonghyeon@users.noreply.github.com>
2024-07-15 20:36:46 +0900

    Title: 컨베이어 벨트, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 240715/컨베이어 벨트/conveyor-belt.py
+++ 240715/컨베이어 벨트/conveyor-belt.py
@@ -1,21 +1,20 @@
 n, t = map(int, input().split())
 upper_list = list(map(int, input().split()))
 lower_list = list(map(int, input().split()))
-lower.reverse()
 
 
 def simulate(sec):
     for _ in range(sec):
         tmp_u = upper_list[-1]
-        tmp_l = lower_list[0]
+        tmp_l = lower_list[-1]
 
         for i in range(n-1, 0, -1):
             upper_list[i] = upper_list[i-1]
         upper_list[0] = tmp_l
 
-        for i in range(1, n, -1):
-            upper_list[i-1] = upper_list[i]
-        upper_list[0] = tmp_u
+        for i in range(n-1, 0, -1):
+            lower_list[i] = lower_list[i-1]
+        lower_list[0] = tmp_u
 
 
 simulate(t)
