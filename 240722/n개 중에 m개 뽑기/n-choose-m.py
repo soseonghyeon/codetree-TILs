52a8f2b0747b7e7787403c5be99555cbaa8944b6
soseonghyeon <103256915+soseonghyeon@users.noreply.github.com>
2024-07-22 21:40:26 +0900

    Title: n개 중에 m개 뽑기, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/n개 중에 m개 뽑기/n-choose-m.py
+++ 240722/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,5 +1,5 @@
 k, n = map(int, input().split())
-arr = []
+arr = [0]
 
 # 직전 수보다 더 큰 수만 append
 # 파라미터로 넘겨도 되지만, arr의 마지막 원소 확인해도 됨
@@ -7,14 +7,10 @@ def choose_number_cond(count):
     global k, n
 
     if count > n:
-        print(*arr)
+        print(*arr[1:])
         return
     
-    for i in range(1, k+1):
-        if i in arr:
-            continue
-        if count >= 2 and i <= arr[-1]:
-            continue
+    for i in range(arr[-1]+1, k+1):
         arr.append(i)
         choose_number_cond(count+1)
         arr.pop()
