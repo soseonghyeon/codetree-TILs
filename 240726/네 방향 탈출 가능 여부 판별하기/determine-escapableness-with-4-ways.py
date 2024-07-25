238155b0e34779408e4788e57683d183717a3cc8
soseonghyeon <103256915+soseonghyeon@users.noreply.github.com>
2024-07-26 00:09:34 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 227ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240726/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,34 @@
+from collections import deque
+# queue 모듈은 멀티쓰레딩을 위한 동기화 과정을 거치므로 deque보다 느리다
+# deque의 append(), popleft()를 통해 큐 사용
+
+n, m = map(int, input().split())
+# True면 뱀이 없음. 갈 수 있는 길
+grid = [list(map(lambda x: True if x=='1' else False, input().split()))
+        for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+
+# 상하좌우
+dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+q = deque()
+q.append((0, 0))
+visited[0][0] = True
+
+while q: # while len(q):
+    x, y = q.popleft()
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+        if nx<0 or nx>=n or ny<0 or ny>=m:
+            continue
+        
+        if visited[nx][ny] or not grid[nx][ny]:
+            continue
+        
+        q.append((nx, ny))
+        visited[nx][ny] = True
+
+if visited[n-1][m-1]:
+    print(1)
+else:
+    print(0)
