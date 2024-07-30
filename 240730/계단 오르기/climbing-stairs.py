dfcbb7e919ad233dcae72421f3270909fad0f875
soseonghyeon <103256915+soseonghyeon@users.noreply.github.com>
2024-07-30 17:46:04 +0900

    Title: 계단 오르기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/계단 오르기/climbing-stairs.py
+++ 240730/계단 오르기/climbing-stairs.py
@@ -1,14 +1,20 @@
 n = int(input())
 
 step = [0 for _ in range(n+1)]
+M = 10007
+
+step[2] = 1
+if n >= 3:
+    step[3] = 1
 
 # 0 ~ n-2까지 
 for i in range(n-1):
-    if step[i+2] == 0:
-        step[i+2] = step[i]+1
+    step[i+2] += step[i]
+    step[i+2] %= M
     
-    if i < n-2 and step[i+3] == 0:
-        step[i+3] = step[i]+1
+    if i < n-2:
+        step[i+3] += step[i]
+        step[i+3] %= M
 
 
 print(step[n])
