284ad3923c30cd9627c54781abf894db0b35c277
soseonghyeon <103256915+soseonghyeon@users.noreply.github.com>
2024-08-03 17:20:11 +0900

    Title: 숫자 전쟁, Time: 174ms, Memory: 30MB, Status: Passed - CodeTree

--- 240803/숫자 전쟁/number-war.py
+++ 240803/숫자 전쟁/number-war.py
@@ -1,30 +1,28 @@
+import sys
+
 n = int(input())
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
 
-dp = [[0]*(n+1) for _ in range(n+1)]
+MIN_VALUE = -sys.maxsize+1
+dp = [[MIN_VALUE]*(n+1) for _ in range(n+1)]
+dp[0][0] = 0
 
-black_row = []
-black_col = []
-
+start_idx = 0
 for i in range(n):
     for j in range(n):
-        if i in black_row or j in black_col:
+        if dp[i][j] == MIN_VALUE:
             continue
 
         # 두 값이 같으면 둘 모두 버려야만 함
-        # 둘 모두 카드를 넘기므로 이후 해당 행과 열은 전파 불가
         if arr2[i] == arr1[j]:
-            black_row.append(i)
-            black_col.append(j)
-
+            dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j])
         # 민우 카드가 버려짐: 아래 방향으로 전파, 민우 점수 획득
-        if arr2[i] < arr1[j]:
+        elif arr2[i] < arr1[j]:
             dp[i+1][j] = max(dp[i+1][j], dp[i][j]+arr2[i])
         # 상대 카드가 버려짐: 오른쪽 방향으로 전파
-        if arr2[i] > arr1[j]:
+        elif arr2[i] > arr1[j]:
             dp[i][j+1] = max(dp[i][j+1], dp[i][j])
-
 
         # 카드 버리기: 그냥 버리는 경우
         # 오른쪽 아래 대각선 방향으로 전파
