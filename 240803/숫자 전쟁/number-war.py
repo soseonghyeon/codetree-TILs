e733adac2d326a302afd9101f5e5160283eb8afd
soseonghyeon <103256915+soseonghyeon@users.noreply.github.com>
2024-08-03 16:25:18 +0900

    Title: 숫자 전쟁, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/숫자 전쟁/number-war.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+dp = [[0]*(n+1) for _ in range(n+1)]
+
+for i in range(n):
+    for j in range(n):
+        # 민우 카드가 버려짐: 아래 방향으로 전파, 민우 점수 획득
+        if arr2[i] < arr1[j]:
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j]+arr2[i])
+        # 상대 카드가 버려짐: 오른쪽 방향으로 전파
+        if arr2[i] > arr1[j]:
+            dp[i][j+1] = max(dp[i][j+1], dp[i][j])
+        # 둘 모두 버려짐: 카드 대결 시 값이 같거나, 또는 그냥 버리는 경우
+        # 오른쪽 아래 대각선 방향으로 전파
+        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j])
+
+result = 0
+for i in range(n):
+    result = max(result, dp[i][n])
+
+result = max(result, max(dp[n]))
+
+print(result)
